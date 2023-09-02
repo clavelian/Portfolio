a24d350c0edc356f0af7002d4bcf94c6853da9ca
Clavelian <ianclavel1991@gmail.com>
2023-09-02 20:47:17 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens: {
+      'mobile': {'min':'360px', 'max':'767px'},
+      // => @media (min-width: phone max 428) {...}
+      'tablet': {'min':'768px', 'max':'1023px'},
+      // => @media (min-width: tablet) { ... }
+      'laptop': {'min':'1024px','max':'2400px'},
+      // => @media (min-width: laptop & desktop) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
